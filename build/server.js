e1a7907391ade2cf993a70760e9d4df3d5f7d1b9
zachvalenta <zjv5002@gmail.com>
2018-01-30 23:12:32 -0500

    add webpack, need to add babel-node to webServe NPM script bc now using ES6 module syntax

--- build/server.js
+++ build/server.js
@@ -1,10 +1,18 @@
-var express = require('express');
+var express = require('express'); // think that Node looks into local node_modules first
 var path = require('path');
 var open = require('open');
+import webpack from 'webpack';
+import config from '../webpack.config.dev'
 
-// in course need Babel to use 'const'
-const port = 3000;
+const port = 3000; // in course need Babel to use 'const'
 const app = express();
+const compiler = webpack(config);
+
+// tell Express to use Webpack set up w/ config from webpack.config
+app.use(require('webpack-dev-middleware')(compiler, { // would have thought comma btw 'webpack-dev' and 'compiler'
+  noInfo: true, // why are we writing more config here (vs. the actual config file)?
+  publicPath: config.output.publicPath
+}));
 
 app.get('/', function(req, res) {
   res.sendFile(path.join(__dirname, '../src/index.html'));
---
+++ webpack.config.dev.js
@@ -0,0 +1,23 @@
+import path from 'path';
+
+export default { // exports this god obj
+  debug: true, // debug info on run
+  devtool: 'inline-source-map', // tradeoff: speed vs. ?
+  noInfo: false, // display list of all files bundled
+  entry: [
+    path.resolve(__dirname, 'src/index') // guess Webpack just assumes this is .js file
+  ],
+  target: 'web', // 'node' another option here
+  output: {
+    path: path.resolve(__dirname, 'src'), // won't actually generate output, will serve from memory, simulate existence on disk
+    publicPath: '/',
+    filename: 'bundle.js' // filename of output that we *don't* generate; ??
+  },
+  plugins: [],
+  module: {
+    loaders: [ // how to handle different file types
+      {test: /\.js$/, exclude: /node_modules/, loaders: ['babel']},
+      {test: /\.css$/, loaders: ['style','css']}
+    ]
+  }
+}
