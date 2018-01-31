9456e369672d291f18e24273f4ceb4a81d1eb40d
zachvalenta <zjv5002@gmail.com>
2018-01-31 11:30:11 -0500

    add eslint

--- build/startMsg.js
+++ build/startMsg.js
@@ -1,3 +1,9 @@
+// --> this how you would disable for single file
+/* eslint-disable no-console */
+
 import chalk from 'chalk';
+
+/* to test if eslint watching */
+// if("hello" = true){console.log("hello");}
 
 console.log(chalk.red.bold.underline('hi, zv...'));
